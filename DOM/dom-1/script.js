e70a4eb783c5e0c65a887ecd29a9e35f0017e354
nerolis <yakumoft@gmail.com>
2017-04-10 20:46:54 +0300

    Add files via upload

---
+++ DOM/dom-1/script.js
@@ -0,0 +1,17 @@
+let newElement = document.createElement('div');
+let container = document.getElementById('container');
+
+// Создание элемента. Атр. - для задания №4
+newElement.setAttribute('class', 'inner_block new_background');
+newElement.setAttribute('id', 'inner_' + Date.now());
+newElement.textContent = 'newDiv';
+//newELement
+
+//Функция prepend(аналог JQ, насколько понял)
+function prepend(parentDiv, newElem) {
+  let setElement = parentDiv.firstChild;
+    parentDiv.insertBefore(newElem, setElement);
+}; // prepend. (в ДЗ-2 превратить в рекурсивную)
+
+//Вызов функции
+prepend( container, newElement);
