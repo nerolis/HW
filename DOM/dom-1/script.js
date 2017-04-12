f33dc9368799aea9c35aca5623a7efeea9d37dd7
nerolis <123123123f>
2017-04-12 05:09:47 +0300

    arrow func

--- DOM/dom-1/script.js
+++ DOM/dom-1/script.js
@@ -1,17 +1,18 @@
+// Переменные
 let newElement = document.createElement('div');
 let container = document.getElementById('container');
 
-// Создание элемента. Атр. - для задания №4
+// Создание элемента
 newElement.setAttribute('class', 'inner_block new_background');
 newElement.setAttribute('id', 'inner_' + Date.now());
 newElement.textContent = 'newDiv';
 //newELement
 
-//Функция prepend(аналог JQ, насколько понял)
+//prepend(аналог JQ, насколько понял)
 let prepend = (parentDiv, newElem) => {
     setElement = parentDiv.firstChild;
     parentDiv.insertBefore(newElem, setElement);
 }; // prepend. (в ДЗ-2 превратить в рекурсивную)
 
-//Вызов функции
+//Вызов
 prepend( container, newElement);
