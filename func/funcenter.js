63b5a1266b470856eb60a5ded487bcd35bd05675
nerolis <yakumoft@gmail.com>
2017-04-21 17:25:14 +0300

    first commit

---
+++ func/funcenter.js
@@ -0,0 +1,13 @@
+function createClub(age){
+  return age >= 18 ?
+    (name) => {return 'Yes, ' + name;} :
+    (name) => {return 'No, ' + name;};
+}
+
+  let clubEnter = [];
+  for (let i = 0, x = 50; i < x; i++) {
+    clubEnter[i] = createClub(i);
+  }
+
+  console.log(clubEnter[18] ('Enter'));
+  console.log(clubEnter[17] ('Go away'));
